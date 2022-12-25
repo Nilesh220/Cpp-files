27ca95952c02b012f6ff4bd3621905245edad40a
Nilesh kumar gupta <113936379+Nilesh220@users.noreply.github.com>
2022-12-25 12:18:26 +0530

    Add files via upload

---
+++ if_else.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int savings;
+    cin>>savings;
+
+    if(savings>5000) {
+        cout<<"neha\n";
+} else if(savings>2000) {
+    cout<<"rashmi\n";
+} else {
+    cout<<"friends\n";
+} 
+    return 0;
+}
